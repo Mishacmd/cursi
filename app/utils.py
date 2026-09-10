dfabd90f56f1e97034d221f2b1cb4c9e242eacae
Mishacmd <nedogonovmd@gmail.com>
2026-09-10 20:39:51 +0300

    new utils and tests (#3)
    * new utils and tests
    
    * litle fix
    
    * uncommented tests
    
    ---------
    
    Co-authored-by: Rodvvv <randomemail@gmail.com>

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,6 @@
+import string
+
+
 def calculate_discount(price: float, discount: float) -> float:
     try:
         new_price = round(price * (1 - discount / 100), 2)
@@ -12,3 +15,21 @@ def is_even(number : int) -> bool:
 def get_full_name(first_name : str, last_name : str) -> str:
     full_name = f"{first_name} {last_name}"
     return full_name
+
+def is_password_strong(password: str) -> bool:
+    if len(password) < 8:
+        return False
+
+    if not any(char in string.punctuation for char in password):
+        return False
+
+    if any(char in string.whitespace for char in password):
+        return False
+    
+    if not any(char.isupper() for char in password) or not any(char.islower() for char in password):
+        return False
+    
+    if not any(char.isdigit() for char in password):
+        return False
+    
+    return True
