38e854fe9b9356100cc1343ec3c3db982431b698
Rodvvv <randomemail@gmail.com>
2026-09-10 21:40:04 +0300

    shopping cart

---
+++ app/project/shoping_cart.py
@@ -0,0 +1,20 @@
+class ShoppingCart:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, item : str, price : float | int, quantity : int = 1):
+        if item in [i["item"] for i in self.items]:
+            for i in self.items:
+                if i["item"] == item:
+                    i["quantity"] += quantity
+                    i["price"] = price
+                    break
+        else:
+            self.items.append({ "item": item, "price": price, "quantity": quantity })
+
+    def remove_item(self, item):
+        if item in self.items:
+            self.items.remove(next(i for i in self.items if i["item"] == item))
+
+    def get_total(self):
+        return sum(item["price"] * item["quantity"] for item in self.items)
